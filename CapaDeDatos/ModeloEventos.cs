c120557b298258c0ac05fe85ce3e72929a6785fe
StivenArtigas <stiven.artigas@estudiante.ceibal.edu.uy>
2024-08-30 17:26:10 -0300

    se realiza metodo crear evento

---
+++ CapaDeDatos/ModeloEventos.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Modelo
+{
+    public class ModeloEventos : Modelo
+    {
+        public int id_evento;
+        public string nombre_evento;
+        public string informacion;
+        public string lugar;
+
+        public void CrearEvento()
+        {
+            string sql = $"insert into eventos (nombre_evento, informacion, lugar) values(@nombre_evento, @informacion, @lugar)";
+            this.Comando.Parameters.AddWithValue("@nombre_evento", nombre_evento);
+            this.Comando.Parameters.AddWithValue("@informacion", informacion);
+            this.Comando.Parameters.AddWithValue("@lugar", lugar);
+            this.Comando.Prepare();
+            this.Comando.CommandText = sql;
+            this.Comando.ExecuteNonQuery();
+        }
+    }
+}
