2896d1b061c8e214e2daf3d58ebd96a190334df1
StivenArtigas <stiven.artigas@estudiante.ceibal.edu.uy>
2024-08-30 17:47:30 -0300

    se crea controlador crear evento

---
+++ CapaLogica/ControladorEventos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Modelo;
+
+namespace CapaLogica
+{
+    class ControladorEventos
+    {
+        public static void CrearEvento(String nombre_evento, string informacion, string lugar, string fecha)
+        {
+            ModeloEventos CrearEvento = new ModeloEventos();
+            CrearEvento.nombre_evento = nombre_evento;
+            CrearEvento.informacion = informacion;
+            CrearEvento.lugar = lugar;
+            CrearEvento.fecha = fecha;
+            CrearEvento.CrearEvento();
+        }
+    }
+}
