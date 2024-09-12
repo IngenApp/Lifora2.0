48fde9b5ae819ec7d4b28bd4bdd3f64265fd6503
AndresTrias <otrias@estudiante.ceibal.edu.uy>
2024-09-12 20:30:22 -0300

    se crea proyecto API

---
+++ ApiLifora/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace ApiLifora.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
