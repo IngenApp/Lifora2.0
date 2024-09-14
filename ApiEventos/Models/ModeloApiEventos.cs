f295025bfe92a5ee662c54c74c07faa23c0ddbef
AndresTrias <otrias@estudiante.ceibal.edu.uy>
2024-09-13 23:35:28 -0300

    Se crea ApiEventos

---
+++ ApiEventos/Models/ModeloApiEventos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ApiEventos.Models
+{
+    public class ModeloApiEventos
+    {
+        public int id_evento;
+        public string nombre_evento;
+        public string informacion;
+        public string lugar;
+        public string fecha_evento;
+        public int id_cuenta;
+        public string habilitado;
+    }
+}
