0bbd3cfa1358ebc0a7838f79a69c50cda1935afa
AndresTrias <otrias@estudiante.ceibal.edu.uy>
2024-09-16 20:55:13 -0300

    se crea funcion Api Listar Grupos

--- ApiGrupo/Controllers/GrupoController.cs
+++ ApiGrupo/Controllers/GrupoController.cs
@@ -2,10 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-
+using ApiGrupo.Models;
+using System.Web.Http;
+using Controladores;
+using System.Data;
 namespace ApiGrupo.Controllers
 {
-    public class GrupoController
+    public class GrupoController : ApiController
     {
+        [Route("api/Grupo/ListarGrupos")]
+        [HttpGet]
+        public List<ModeloApiGrupo> Get()
+        {
+            DataTable grupo = ControladorGrupos.ListarGrupos();
+            List<ModeloApiGrupo> listarGrupos = new List<ModeloApiGrupo>();
+          
+            foreach (DataRow grupos in grupo.Rows)
+            {
+                ModeloApiGrupo mag = new ModeloApiGrupo();
+                mag.idGrupo = Int32.Parse(grupos["idGrupo"].ToString());
+                mag.nombre = grupos["nombre"].ToString();
+                mag.descripcion = grupos["descripcion"].ToString();
+                mag.fecha = grupos["fecha"].ToString();
+                mag.habilitado = bool.Parse(grupos["habilitado"].ToString());
+                listarGrupos.Add(mag);
+            }
+            return listarGrupos;
+        }
     }
 }
